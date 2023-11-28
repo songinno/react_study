2e5d2a4c44cf1007d71d7ccd59efb26861063d45
songinno <songinno@naver.com>
2023-11-29 00:11:44 +0900

    8. 리액트와 타입스크립트 - 컴포넌트 타입 정의 [2023.11.29]

--- chapter8/react-typescript/src/App.tsx
+++ chapter8/react-typescript/src/App.tsx
@@ -1,31 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import axios from "axios";
 import { ListItem } from './components/ListItem';
+import type { User } from './types/user';
 
-// 사용자 정보 타입 정의
-type User = {
-  id: number;
-  name: string;
-  age: number;
-  personalColor: string;
-};
-
-const App = () => {
+export const App = () => {
   // 얻은 사용자 정보
   const [users, setUsers] = useState<User[]>([]);
 
   // 화면에 표시될 때 사용자 정보 얻기
   useEffect(() => {
-    axios.get("https//example.com/users")
+    axios.get<User[]>("https//example.com/users")
         .then((res) => {setUsers(res.data)});
   }, [])
 
   return (
     <div>
       {users.map(user => (
-        <ListItem id={user.id} name={user.name} age={user.age} />
+        <ListItem id={user.id} name={user.name} age={user.age} personalColor={user.personalColor} />
       ))}
     </div>
   );
--- chapter8/react-typescript/src/components/ListItem.tsx
+++ chapter8/react-typescript/src/components/ListItem.tsx
@@ -1,7 +1,10 @@
-export const ListItem = props => {
-    const {id, name, age} = props;
+import type { FC } from "react";
+import type { User } from "../types/user";
+
+export const ListItem: FC<User> = props => {
+    const {id, name, age, personalColor} = props;
     return (
-        <p>
+        <p style={{color: personalColor}}>
             {id}:{name}({age})
         </p>
     );
